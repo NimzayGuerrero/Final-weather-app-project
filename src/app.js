7e52c55f2536124f38af8ec47ba0ff1925f57b90
NimzayGuerrero <75545690+NimzayGuerrero@users.noreply.github.com>
2020-12-21 02:24:13 +0100

    update main weather icon con api

--- src/app.js
+++ src/app.js
@@ -27,7 +27,12 @@ function displayTemperature (response){
     currentCity.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
     let currentDayTime = document.querySelector("#day-time");
     currentDayTime.innerHTML = formatDay(response.data.dt * 1000);
+    let currentIcon = document.querySelector("#icon");
+    currentIcon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    currentIcon.setAttribute("alt",`${response.data.weather[0].description}`);
+
 }
+let city = "Caracas"
 let apiKey = "57c364089b821cdc467408c870253e67";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Caracas&appid=${apiKey}&units=metric`;
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiURL).then(displayTemperature);
